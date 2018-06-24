39c58ea26f0abf0c6e3af45573d68df5560d267d
hadi tok <hadi@citizenme.com>
2018-06-24 15:38:22 +0300

    added kotlin support and added SAM for Jockey.on() method

---
+++ JockeyJSAndroid/jockeyJS/src/main/java/com/jockeyjs/Extensions.kt
@@ -0,0 +1,9 @@
+package com.jockeyjs
+
+private fun Jockey.on(on: String, function: (payload: Map<Any, Any>) -> Unit) {
+    this.on(on, object : JockeyHandler() {
+        override fun doPerform(payload: Map<Any, Any>) {
+            function(payload)
+        }
+    })
+}
